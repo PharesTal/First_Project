0c6359c05e6d16c8b57eda5be76dc78179009a98
PharesTal <tallevi1703@gmail.com>
2024-12-23 10:53:16 +0200

    change1

--- main_file.py
+++ main_file.py
@@ -1,1 +1,39 @@
-hi
+import system_data
+def main():
+    while True:
+        print("\nMenu:")
+        print("1. get cpu info")
+        print("2. get memory info")
+        print("3. get disk info")
+        print("4. Option 4")
+        print("5. Option 5")
+        print("6. Option 6")
+        print("7. Option 7")
+        print("8. Option 8")
+        print("9. Exit")
+        choice = input("Please select an option (1-9): ")
+        if choice == '1':
+            system_data.get_cpu_info()
+        elif choice == '2':
+            print(f"{system_data.get_memory_info()}")
+        elif choice == '3':
+            print(f"{system_data.get_disk_info()}")
+        elif choice == '4':
+            print("You selected Option 4.")
+        elif choice == '5':
+            print("You selected Option 5.")
+        elif choice == '6':
+            print("You selected Option 6.")
+        elif choice == '7':
+            print("You selected Option 7.")
+        elif choice == '8':
+            print("You selected Option 8.")
+        elif choice == '9':
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
--- system_data.py
+++ system_data.py
@@ -1,12 +1,16 @@
 import psutil
 
+
 def get_cpu_info():
     cpu_usage = psutil.cpu_percent(interval=1)
     logical_cores = psutil.cpu_count(logical=True)
     physical_cores = psutil.cpu_count(logical=False)
     cpu_freq = psutil.cpu_freq()
     cpu_times = psutil.cpu_times()
-    return cpu_usage, logical_cores, physical_cores, cpu_freq, cpu_times
+    for name, value in locals().items():
+        print(f"{name}: {value}")
+
+    return locals()
 
 def get_memory_info():
     virtual_memory = psutil.virtual_memory()
@@ -19,12 +23,19 @@ def get_memory_info():
     used_swap = swap_memory.used / (1024 ** 3)
     free_swap = swap_memory.free / (1024 ** 3)
     swap_usage = swap_memory.percent
-    return total_memory, available_memory, used_memory, memory_usage, total_swap, used_swap, free_swap, swap_usage
+    for name, value in locals().items():
+        print(f"{name}: {value}")
+
+    return locals()
+
 def get_disk_info():
     disk_usage = psutil.disk_usage('/')
     total_disk = disk_usage.total / (1024 ** 3)
     used_disk = disk_usage.used / (1024 ** 3)
     free_disk = disk_usage.free / (1024 ** 3)
     percent_used = disk_usage.percent
-    return total_disk, used_disk, free_disk, percent_used
+    for name, value in locals().items():
+        print(f"{name}: {value}")
+
+    return locals()
 
